1e1010dfe6079a9fdff8028c832622f5badc42ff
Danila Martiuk <danya.mart1985@gmail.com>
2018-03-13 22:47:00 +0200

    doing text and button use Js

---
+++ JS/01. Sorax/sorax34.js
@@ -0,0 +1,10 @@
+var links,addEventListener ;
+
+links = {
+    main: "This is the <strong>main</strong> page",
+    about: "This is the <strong>about</strong> page",
+    dowloads: "This is the <strong>dowloads</strong> page
+
+window.addEventListener('hashchange' updatestate);
+
+}
