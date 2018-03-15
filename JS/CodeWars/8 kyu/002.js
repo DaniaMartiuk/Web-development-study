d673c95b141ce0e66c1ea0587b910856e5474ca5
Danila Martiuk <danya.mart1985@gmail.com>
2018-03-15 21:50:37 +0200

    doing Text with use small leter with big use JavaScript

---
+++ JS/CodeWars/8 kyu/002.js
@@ -0,0 +1,7 @@
+function capitalizeWord(Word) {
+    var blala = Word[0].toUpperCase() + Word.substr(1);
+    return blala;
+}
+// -----------------------------
+var a = capitalizeWord("dania");
+console.log("a = " + a);
