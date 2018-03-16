7f59969fc90a7417ca75e2fdcbd0d48cffdec80f
Danila Martiuk <danya.mart1985@gmail.com>
2018-03-16 22:58:54 +0200

    doing cycle use massif

---
+++ JS/CodeWars/8 kyu/003.js
@@ -0,0 +1,35 @@
+// function functionMachine (arrValue) {
+//     for()
+//     return arrValue;
+// }
+
+// var z = functionMachine(arr);
+// console.log(z);
+
+var arr = [
+    [0, 5, 55, 33],
+    [55, 44, 88, 11, 1],
+    "Dania",
+    15,
+    [2, -8],
+    11
+];
+
+// console.log(arr[4]);
+// console.log(arr.length);
+
+// var a = 7;
+// a++;    // a = a + 1
+// a--;    // a = a - 1
+// console.log(a);
+
+// for(start, finish, step);    // проходить по массиву
+for (var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
