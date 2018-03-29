04e2210681d4a56622006f5c8b9af46531c69804
Danila Martiuk <danya.mart1985@gmail.com>
2018-03-29 21:38:50 +0300

    connected two text made text and connected the two text made one text and made and combined textЭ

---
+++ JS/FreeCodeCamp/JavaScript/028.js
@@ -0,0 +1,8 @@
+
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Hello ";
+var myStr = 'Hello' + myName + 'What your name?';
---
+++ JS/FreeCodeCamp/JavaScript/029.js
@@ -0,0 +1,10 @@
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "Free Code Camp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+var someAdjective  = "cool!";
+var myStr  = "Free Code Camp is free and ";
+myStr  += someAdjective;
