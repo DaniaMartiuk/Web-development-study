52c124ac9f3be93930a95dd668a229d384be780c
Danila Martiuk <danya.mart1985@gmail.com>
2018-04-04 19:06:31 +0300

    Data with Indexes and doing Modify Array Data With Indexes and doing Access MultiDimensional Arrays With Indexes

---
+++ JS/FreeCodeCamp/JavaScript/046.js
@@ -0,0 +1,13 @@
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = ["John", 23];
+
+
---
+++ JS/FreeCodeCamp/JavaScript/047.js
@@ -0,0 +1,13 @@
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["Paul", 35], ["dog", 3]];
+
+// Only change code below this line.
+
+
