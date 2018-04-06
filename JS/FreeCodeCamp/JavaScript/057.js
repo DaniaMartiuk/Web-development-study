9c2eff54e44d0242351af9c99726a271c8a346e9
Danila Martiuk <danya.mart1985@gmail.com>
2018-04-06 20:42:28 +0300

    Understanding Boolean Values and doing Use Conditional Logic with If Statements

---
+++ JS/FreeCodeCamp/JavaScript/057.js
@@ -0,0 +1,27 @@
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+    // Only change code below this line.
+
+
+
+    // Only change code above this line.
+
+}
+
+// Change this value to test
+function trueOrFalse(True) {
+    if (True) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+}
