acdf2e75190f8089f404b1d3c083dad4289d43b4
Danila Martiuk <danya.mart1985@gmail.com>
2018-04-17 21:57:12 +0300

    Make Instances of Objects with a Constructor Function and Make Unique Objects by Passing Parameters to our Constructor

---
+++ JS/FreeCodeCamp/JavaScript/113.js
@@ -0,0 +1,19 @@
+
+var Car = function() {
+    //Change this constructor
+    this.wheels = 4;
+    this.seats = 5;
+    this.engines = 1;
+};
+
+//Try it out here
+
+var Car = function(wheels, seats, engines) {
+
+    this.wheels = wheels;
+
+    this.seats = seats;
+
+    this.engines = engines;
+
+};
