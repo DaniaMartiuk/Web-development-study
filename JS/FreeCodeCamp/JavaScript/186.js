addb337bba59a8cef953a133e322f9115f47dc29
Danila Martiuk <danya.mart1985@gmail.com>
2018-04-20 23:00:46 +0300

    Factorialize a Number and Check for Palindromes and Find the Longest Word in a String and Title Case a Sentence

---
+++ JS/FreeCodeCamp/JavaScript/186.js
@@ -0,0 +1,9 @@
+
+function palindrome(str) {
+    // Good luck!
+    return str.replace(/[\W_]/g, '').toLowerCase() ===
+        str.replace(/[\W_]/g, '').toLowerCase().split('').reverse('').join('');
+}
+
+
+palindrome("eye");
