cadd9a529abc8f209061747c32e2826f5c9ec7d8
Danila Martiuk <danya.mart1985@gmail.com>
2018-05-31 10:32:24 +0300

    do controle city and do var with string and isReallyNaN

--- JS/FreeCodeCamp/JavaScript/261.js
+++ JS/FreeCodeCamp/JavaScript/261.js
@@ -1,1 +1,15 @@
-var a = "123";
+function Counter() {
+    this.value = 0;
+}
+
+Counter.prototype.increase = function() {
+    this.value++;
+};
+
+Counter.prototype.getValue = function() {
+    return this.value;
+};
+
+Counter.prototype.reset = function() {
+    this.value = 0;
+};
